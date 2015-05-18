f6323e371bfb19922cf6877870c4fecdeb839fc4
R4YM3 <raymond@hetwebbureau.nl>
2015-05-18 22:42:34 +0200

    Updated JS paths

--- src/js/config.js
+++ src/js/config.js
@@ -3,18 +3,18 @@
 require.config({
     baseUrl: '../js/app',
     paths: {
-        jquery: '../../components/jquery/dist/jquery',
-        modernizr: '../../components/modernizr/modernizr',
+        jquery: '../../../components/jquery/dist/jquery',
+        modernizr: '../../../components/modernizr/modernizr',
         respond: '/static/wtf/components/respond/respond.min',
         requireLib: '/static/wtf/components/requirejs/require',
-        html5shiv: '../../components/html5shiv/dist/html5shiv',
-        requirejs: '../../components/requirejs/require',
-        'sticky-kit': '../../components/sticky-kit/jquery.sticky-kit',
-        sticky: '../../components/sticky/index',
-        FlowTypeJS: '../../components/FlowTypeJS/flowtype',
-        fitvids: '../../components/fitvids/jquery.fitvids',
-        fontawesome: '../../components/fontawesome/fonts/*',
-        'fontface-novecento-wide': '../../components/fontface-novecento-wide/fonts/**/*'
+        html5shiv: '../../../components/html5shiv/dist/html5shiv',
+        requirejs: '../../../components/requirejs/require',
+        'sticky-kit': '../../../components/sticky-kit/jquery.sticky-kit',
+        sticky: '../../../components/sticky/index',
+        FlowTypeJS: '../../../components/FlowTypeJS/flowtype',
+        fitvids: '../../../components/fitvids/jquery.fitvids',
+        fontawesome: '../../../components/fontawesome/fonts/*',
+        'fontface-novecento-wide': '../../../components/fontface-novecento-wide/fonts/**/*'
     },
     shim: {
         jquery: {
