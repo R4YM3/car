deab0e83fee6dbbe871c38bee70d1c4bfa553079
R4YM3 <raymond@hetwebbureau.nl>
2015-05-18 22:43:34 +0200

    Removed unessary js and added js-hide

--- src/js/app/views/global.js
+++ src/js/app/views/global.js
@@ -1,24 +1,5 @@
 define(["jquery"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     'use strict';
 
-    $(document).ready(function() {
-
-        $(".js-hide").hide();
-
-        $('.search.input_text').bind('blur', function() {
-            $(".search.block").removeClass("focus");
-            $(".followus.block").removeClass("hide");
-        });
-
-        $('.search.input_text').bind('focus', function() {
-            $(".search.block").addClass("focus");
-            $(".followus.block").addClass("hide");
-        });
-
-        if (code == 27) { // Escape
-            $("input_text").off('blur'); // unbind the blur event
-        }
-    });
-
+    $("js-hide").hide();
 });
