047a2748e32a0462e2beff7facfb72db26df2043
R4YM3 <raymond@hetwebbureau.nl>
2015-05-18 20:45:07 +0200

    New bower and require setup

--- src/js/app/views/home.js
+++ src/js/app/views/home.js
@@ -1,4 +1,4 @@
-define(['jquery'], function($) {
+define(['jquery', 'sticky-kit'], function($) {
     'use strict';
 
     $("#foo").text("world!");
--- src/js/config.js
+++ src/js/config.js
@@ -5,15 +5,16 @@ require.config({
     paths: {
         jquery: '../../components/jquery/dist/jquery',
         modernizr: '../../components/modernizr/modernizr',
-        respond: '../../../components/respond/respond.min',
-        requireLib: '../../../components/requirejs/require',
-        fontawesome: '../../components/fontawesome/fonts/*',
-        'fontface-novecento-wide': '../../components/fontface-novecento-wide/fonts/**/*',
+        respond: '/static/wtf/components/respond/respond.min',
+        requireLib: '/static/wtf/components/requirejs/require',
         html5shiv: '../../components/html5shiv/dist/html5shiv',
         requirejs: '../../components/requirejs/require',
         'sticky-kit': '../../components/sticky-kit/jquery.sticky-kit',
+        sticky: '../../components/sticky/index',
         FlowTypeJS: '../../components/FlowTypeJS/flowtype',
-        fitvids: '../../components/fitvids/jquery.fitvids'
+        fitvids: '../../components/fitvids/jquery.fitvids',
+        fontawesome: '../../components/fontawesome/fonts/*',
+        'fontface-novecento-wide': '../../components/fontface-novecento-wide/fonts/**/*'
     },
     shim: {
         jquery: {
@@ -21,6 +22,18 @@ require.config({
         },
         modernizr: {
             exports: 'Modernizr'
+        },
+        'sticky-kit': {
+            deps: [
+                'jquery'
+            ],
+            exports: 'sticky-kit'
+        },
+        sticky: {
+            deps: [
+                'jquery'
+            ],
+            exports: 'sticky'
         }
     },
     packages: [
@@ -33,6 +46,6 @@ require.config({
 //     'menu'
 // ]);
 
-require(['helpers/alert'], function(Y) {
-    Y.alert('alert');
-});
+// require(['helpers/alert'], function(Y) {
+//     Y.alert('alert');
+// });
