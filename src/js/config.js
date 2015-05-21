b34a855cd3bd8e31cbab5232de71c3f750390d87
R4YM3 <raymond@hetwebbureau.nl>
2015-05-21 11:06:45 +0200

    placed components dir into a var

--- src/js/config.js
+++ src/js/config.js
@@ -1,20 +1,22 @@
 'use strict';
 
+var componentsDir = '../../../';
+
 require.config({
     baseUrl: '../js/app',
     paths: {
-        jquery: '../../../components/jquery/dist/jquery',
-        modernizr: '../../../components/modernizr/modernizr',
+        jquery: componentsDir + 'components/jquery/dist/jquery',
+        modernizr: componentsDir + 'components/modernizr/modernizr',
         respond: '/static/wtf/components/respond/respond.min',
         requireLib: '/static/wtf/components/requirejs/require',
-        html5shiv: '../../../components/html5shiv/dist/html5shiv',
-        requirejs: '../../../components/requirejs/require',
-        'sticky-kit': '../../../components/sticky-kit/jquery.sticky-kit',
-        sticky: '../../../components/sticky/index',
-        FlowTypeJS: '../../../components/FlowTypeJS/flowtype',
-        fitvids: '../../../components/fitvids/jquery.fitvids',
-        fontawesome: '../../../components/fontawesome/fonts/*',
-        'fontface-novecento-wide': '../../../components/fontface-novecento-wide/fonts/**/*'
+        html5shiv: componentsDir + 'components/html5shiv/dist/html5shiv',
+        requirejs: componentsDir + 'components/requirejs/require',
+        'sticky-kit': componentsDir + 'components/sticky-kit/jquery.sticky-kit',
+        sticky: componentsDir + 'components/sticky/index',
+        FlowTypeJS: componentsDir + 'components/FlowTypeJS/flowtype',
+        fitvids: componentsDir + 'components/fitvids/jquery.fitvids',
+        fontawesome: componentsDir + 'components/fontawesome/fonts/*',
+        'fontface-novecento-wide': componentsDir + 'components/fontface-novecento-wide/fonts/**/*'
     },
     shim: {
         jquery: {
